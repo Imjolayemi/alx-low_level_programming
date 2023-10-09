81366a3e606cf83fc3df5cce7e6cee23c1faac8c
imjolayemi <imlandlord1@gmail.com>
2023-10-09 23:02:15 +0100

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always Zero(0) Success
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
