b3ba015d82870a08737845d23af16037adc48f94
imjolayemi <imlandlord1@gmail.com>
2023-10-09 22:24:54 +0100

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always Zero(0) Success
+ */
+
+int main(void)
+{
+	char a = 'a';
+	
+	for (; a <= 'z'; a++)
+	{
+		if ((a == 'q') || (a == 'e'))
+		{
+			continue;
+		}
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
