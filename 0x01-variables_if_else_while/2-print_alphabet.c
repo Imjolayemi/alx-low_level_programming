4b7c7a708c6398a8295a6b88bd64f2a457866674
imjolayemi <imlandlord1@gmail.com>
2023-10-09 18:28:05 +0100

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always Zero(0) Success
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	for (; a <='z'; a++)
+	{
+		putchar(a);
+		putchar('\n');
+	}
+	return (0);
+}
