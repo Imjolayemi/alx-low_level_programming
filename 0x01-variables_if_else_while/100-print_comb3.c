354db48667df734e2c286d99da202a277aeae59a
imjolayemi <imlandlord1@gmail.com>
2023-10-10 11:19:00 +0100

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always zero(0) Success
+ */
+
+int main(void)
+{
+	int a = 48;
+	int b;
+
+	for (; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			if (a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if ((b == 57) && (a == 56))
+				{
+					break;
+				}
+				putchar(44);
+				putchar(32);
+			}
+			
+		}
+
+	}
+	putchar(10);
+	return (0);
+}
