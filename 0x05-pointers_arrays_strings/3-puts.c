c0cdb2f504ecd38958238deab97837e9235981a5
imjolayemi <imlandlord1@gmail.com>
2023-10-17 13:28:48 +0100

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - function that return a string
+ * @str: string that will be printed
+ */
+
+/* Declaration of a function */
+void _puts(char *str)
+{
+	puts(str);
+}
