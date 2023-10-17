a9c426fa64d797e69a57b24741674377057246ab
imjolayemi <imlandlord1@gmail.com>
2023-10-17 14:03:28 +0100

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always Zero(0) Success
+ */
+
+int main(void)
+{
+	char *str;
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - function that hold the reverse of a string
+ * @s: Reverse string
+ */
+
+/* Declaration of function */
+void print_rev(char *s)
+{
+	int c;
+
+	c = 0;
+	for (; s[c] != '\0'; c++);
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar(10);
+}
