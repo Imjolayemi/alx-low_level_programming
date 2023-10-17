db327fb167840bf7c8ceab533e29486cf7d4e809
imjolayemi <imlandlord1@gmail.com>
2023-10-17 08:49:34 +0100

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
+ * reset_to_98 - Holding the value of n as pointer
+ * @*n: pointer variable having the value of ninty-eight(98)
  */
 
+/*Declaration of the function reset_to_98*/
 void reset_to_98(int *n)
 {
 	*n = 98;
