7f29de983a298a68689c791687cd3c63e08141e5
imjolayemi <imlandlord1@gmail.com>
2023-10-17 11:38:21 +0100

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,6 +3,9 @@
 /**
  * _strlen - The function to know the length of a string
  * @s: string to know the length
+ * @length: Determine the length of the string
+ *
+ * Return: Always return the value of length if successfull
  */
 
 /* Declaration of function */
