9ba469d5e252dd9ff228593e404b34679aa7bfd4
imjolayemi <imlandlord1@gmail.com>
2023-10-18 01:24:26 +0100

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Reversing the string to stdio
+ * @s: String that will be reversed
+ */
+
+/* Declaration of function */
+void rev_string(char *s)
+{
+	int c, i;
+	char rev = s[0];
+
+	c = 0;
+	while (s[c] != '\n')
+	{
+		c++;
+	}
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		rev = s[i];
+		s[i] = s[c];
+		s[c] = rev;
+	}
+}
